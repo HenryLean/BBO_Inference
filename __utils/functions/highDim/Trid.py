d2be005fdb341b652deb4f570092ede8dca4a6b9
Henry <henrylean@outlook.com>
2024-07-11 17:20:45 +0800

    minor modifications

--- __utils/functions/highDim/Trid.py
+++ __utils/functions/highDim/Trid.py
@@ -3,7 +3,7 @@ def fnTrid(x, scale=1, intercept=0, *args, **kwds):
     """
     http://www.sfu.ca/~ssurjano/trid.html
     domain = [-d**2, d**2]
-    optimum = -d*(d+4)*(d-1), optimizer = [(i+1)*(d-i) for i in range(d)]
+    optimum = -d*(d+4)*(d-1)/6, optimizer = [(i+1)*(d-i) for i in range(d)]
     """
     d = len(x)
     x = np.array(x, dtype=float)
--- adversary/binary.py
+++ adversary/binary.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 class BinaryEnv(object):
-    def __init__(self, meanFn, *fn, **kw):
+    def __init__(self, meanFn, epsilon=1e-6, *fn, **kw):
         self.mean = meanFn
+        self.eps = epsilon
 
     def get_obs(self, action, size=1, *args, **kwargs):
-        mean = max(0, min(1, self.mean(action)/5))
+        mean = max(self.eps, min(1-self.eps, self.mean(action)))
         rvs = np.random.binomial(1, mean, size=size)
         return rvs
