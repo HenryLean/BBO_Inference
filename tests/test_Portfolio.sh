6afac2d93d54dc6ed91f60901fd441a4d9580c66
Henry <henrylean@outlook.com>
2025-09-27 12:01:44 +0800

    Update test_Portfolio.sh

--- tests/test_Portfolio.sh
+++ tests/test_Portfolio.sh
@@ -3,7 +3,7 @@ ALGORITHMS=("spsa-const" "spsa-ordin" "spsa-multi" "spsa1s-ordin" "4point-ordin"
 
 for alg in "${ALGORITHMS[@]}"; do
     for method in "${METHODS[@]}"; do
-        ### 
+        ### By default --env_config = "./_configs/env_config_default2.json", and it is the setting for zero mean
         python /Users/henrylean/Gits/bbossi/main.py --algorithm "$alg" --alg_config ./_configs/alg_config_default2.json --num_replication 300 --len_iteration 102400 --folder "./results/$method" --est_var_method "$method" 
         ### 
         # python /Users/henrylean/Gits/bbossi/main.py --algorithm "$alg" --alg_config ./_configs/alg_config_default2.json --env_config ./_configs/env_config_portfolio.json --num_replication 300 --len_iteration 102400 --folder "./results/$method" --est_var_method "$method" 
